d7f694bcf3125264c0e4acb3cc66986ac727c792
Iris  Ponce <irisponce@Iriss-MacBook-Pro.local>
2023-08-07 21:59:15 -0400

    Added the the ability to give the directory, type of data (SPE vrs tile run) and number of active channels as arguments for the code

--- StandAlone_DataAnalyzer.py
+++ StandAlone_DataAnalyzer.py
@@ -21,6 +21,9 @@ import scipy.signal
 from lmfit.models import GaussianModel, ConstantModel
 from datetime import datetime
 from glob import glob
+import argparse
+import shutil
+import pylandau
 
 
 
@@ -37,12 +40,13 @@ ped = 50 #pedestal correction
 
 
 def parseData(fpath,chan):
+    print (type(chan))
     if (os.path.isdir(fpath)):
         sourceFolder = fpath
         onlyfiles = [f for f in os.listdir(sourceFolder) if '.dat' in f]
         data_list = []
         for file in onlyfiles:
-            data_list.append(dt5202_crunch(str(sourceFolder)+str(file), num_ev=10000000, num_ch=chan))
+            data_list.append(dt5202_crunch(str(sourceFolder)+str(file), num_ev=10000000, num_ch=int(chan)))
         
         data = data_list[0]
         for i in range(1, len(data_list)):
@@ -153,6 +157,7 @@ def CalculateSiPMGain(data,peakfinderWidth,directory,plotName,counter):
          histtype='step', density  = True)
     centers = (bins[:-1] + bins[1:])/2
     yhat = savitzky_golay(content, 51, 3)
+    print("done filtering the data")
     
     #plt.plot(centers,yhat, color='red')
     #plt.xlim(0,np.max(data['data']['high_gain']))
@@ -172,15 +177,20 @@ def CalculateSiPMGain(data,peakfinderWidth,directory,plotName,counter):
     x = np.linspace(0,np.max(data['data']['high_gain']), len(content))
     r, cov = scipy.optimize.curve_fit(sps, centers, content, p0 = [0, np.median(peak_dist), 5, 5, np.median(data['data']['high_gain'])/np.median(peak_dist)])
     multigauss, peaks =  sps(x, *r, output_single_peaks=True)
-    '''
-    plt.plot(x, multigauss, color='C1')
+    print ("Done with the first fir with same amplitudes ")
+    
+    plt.plot(x, multigauss, color='C2',label = f'Multi-Gauss Fit, gain = {r[1]:.2f} ADC/p.e.')
     for peak in peaks:
         plt.plot(x, peak, '--', color = 'C1')
-    plt.plot(x, sps(x, *r))
+    plt.plot(x, sps(x, *r),color='C3')
+    plt.xlabel('Signal Amplitude in ADC')
+    plt.ylabel('Normalized Counts')
 
+    plt.legend(fontsize = 'x-large')
     plt.figure()
+    plt.savefig(directory + '/' + 'SPE_SPSwithoutSingleGaussians_'+plotName+'.png')
     plt.show()
-    
+    '''
     content, bins, _ = plt.hist(data['data']['high_gain'],bins=np.max(data['data']['high_gain'])
                             ,range=(0,np.max(data['data']['high_gain']+1)),
          histtype='step', density  = True)
@@ -188,7 +198,9 @@ def CalculateSiPMGain(data,peakfinderWidth,directory,plotName,counter):
     centers = (bins[:-1] + bins[1:])/2'''
     r2, cov2 = scipy.optimize.curve_fit(sps_freeamp_fit, centers, content, p0 = [*r[:-1], 0, *poisson_ampls(r[-1])])
     multigauss, peaks =  sps_freeamp(x, *r2, output_single_peaks=True)
-    plt.plot(x, multigauss, color='C1', label = f'Multi-Gauss Fit, gain = {r2[1]:.2f} ADC/p.e.', lw = 3)
+    print ("Doing the free amp fit, almost done")
+    
+    plt.plot(x, multigauss, color='C2', label = f'Multi-Gauss Fit, gain = {r2[1]:.2f} ADC/p.e.', lw = 1)
     for peak in peaks:
         plt.plot(x, peak, '--', color = 'C1')
         
@@ -196,7 +208,7 @@ def CalculateSiPMGain(data,peakfinderWidth,directory,plotName,counter):
     plt.ylabel('Normalized Counts')
 
     plt.legend(fontsize = 'x-large')
-    plt.savefig(directory + '/' + 'SPE_withoSingleGaussians_'+plotName+'.png')
+    plt.savefig(directory + '/' + 'SPE_withoutSingleGaussians_'+plotName+'.png')
     
     plt.figure()
     plt.plot(centers,content)
@@ -217,39 +229,59 @@ def CalculateSiPMGain(data,peakfinderWidth,directory,plotName,counter):
     
     ###
 
-dataFile="/Users/irisponce/Documents/RHI/lfHCal/CorrectSPEfiles-0731/S14160-1315_57Gain_5p5Amplitude_40P0V/"
-data = parseData(dataFile,1)
+##Get the directory fron the arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("inputDirectory",help="Input Directory to analyze")
+parser.add_argument("typeOfData",help="Input type of data to analyze")
+parser.add_argument("activeChannels",help="Number of channels in run")
+#dataFile="/Users/irisponce/Documents/RHI/lfHCal/CorrectSPEfiles-0731/S14160-1315_57Gain_5p5Amplitude_40P0V/"
+args=parser.parse_args()
+activeChannels=int(args.activeChannels)
+dataType=int(args.typeOfData)
+dataFile=str(args.inputDirectory)
 
+print ("Analyzing directory: ",dataFile)
+data = parseData(args.inputDirectory,activeChannels)
+print ("Data parsed, now proceeding to perfrom fits.")
 #data=parseData("/Users/irisponce/Documents/RHI/lfHCal/SPEspectra_07312023/S4K33C0135L-9_57Gain_5p0Amplitude_30P5V/",1)
 
-
-try:
-    r2,percentageErr,percentageErr2,percentageErr3 = CalculateSiPMGain(data,5,dataFile,"try1",0)
-
-    print (percentageErr,percentageErr2,percentageErr3)
-except: 
-    print ("Error ocurred during first fitting, will try again")
-    pass
-
-##I should check here that the values we get from the fitter actually make sense. Because we don't want to 
-#rerun unless it's needed.
-
-if r2[1] <0:
+if dataType==1:
+    print ("Making SPE fits")
     try:
-        print ("Fitting again,we got a negative value for ADC/PE, take 2")
-        r2,percentageErr,percentageErr2,percentageErr3 = CalculateSiPMGain(data,10,dataFile,"try2",0)
-        #print(percentageErr,percentageErr2,percentageErr3)
-    except: 
+        r2,percentageErr,percentageErr2,percentageErr3 = CalculateSiPMGain(data,5,dataFile,"try1",0)
+
+        print (percentageErr,percentageErr2,percentageErr3)
+    except:
+        print ("Error ocurred during first fitting, will try again")
         pass
-    
-if percentageErr3> 4:
-    #try to refit not sure what should change LOL 
-    try:
-        print ("Fitting again, the fitter worked but it wasn't a good fit trying again")
-        r2,percentageErr,percentageErr2,percentageErr3 = CalculateSiPMGain(data,10,dataFile,"try3",0)
-        #print(percentageErr,percentageErr2,percentageErr3)
-    except: 
-        pass
-    
-    
+
+    ##I should check here that the values we get from the fitter actually make sense. Because we don't want to
+    #rerun unless it's needed.
+
+    if r2[1] <0:
+        try:
+            print ("Fitting again,we got a negative value for ADC/PE, take 2")
+            r2,percentageErr,percentageErr2,percentageErr3 = CalculateSiPMGain(data,10,dataFile,"try2",0)
+            print("try 2, ",percentageErr,percentageErr2,percentageErr3)
+        except:
+            pass
+        
+    if abs(percentageErr3)> 4:
+        #try to refit not sure what should change LOL
+        try:
+            print ("Fitting again, the fitter worked but it wasn't a good fit trying again")
+            r2,percentageErr,percentageErr2,percentageErr3 = CalculateSiPMGain(data,15,dataFile,"try3",0)
+            
+            print("try3, ",percentageErr,percentageErr2,percentageErr3)
+        except:
+            pass
+        
+elif dataType==2:
+    print ("Analyzing cosmic data" )
+    print ("STILL NEED TO DO THIS PART")
+    content, bins, _ = plt.hist(data['data']['high_gain'],bins=np.max(data['data']['high_gain'])
+                            ,range=(0,np.max(data['data']['high_gain']+1)),
+                        histtype='step', density  = True)
+    centers = (bins[:-1] + bins[1:])/2
+    #plt.plot()
 
